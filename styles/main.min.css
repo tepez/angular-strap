8ce7cfb54b851091d939df9baa4b1abbfbc90a83
Olivier Louvignes <olivier@mg-crea.com>
2014-06-23 01:41:49 +0200

    chore(pages): fix plunkers

--- styles/main.min.css
+++ styles/main.min.css
@@ -1,6 +1,6 @@
 /**
  * angular-strap
- * @version v2.0.3 - 2014-06-20
+ * @version v2.0.3 - 2014-06-22
  * @link http://mgcrea.github.io/angular-strap
  * @author Olivier Louvignes (olivier@mg-crea.com)
  * @license MIT License, http://www.opensource.org/licenses/MIT
